1d14025a45d4a923a65b60b9e2e7a71639260ccd
Matsun Mint <chocomnt242@gmail.com>
2020-09-11 06:54:29 +0900

    微調整

--- main.py
+++ main.py
@@ -4,22 +4,38 @@ def devide():
 
     coldest_temp = float(input("最寒月の平均気温は？(℃)"))
 
+    warmest_temp = float(input("最暖月の平均気温は？(℃)"))
+    
     precipitation = float(input("年間降水量は？(mm)"))
     if precipitation < 0:
         return f"Err: {err_msg}"
 
-    warmest_temp = float(input("最暖月の平均気温は？(℃)"))
-
     min_rainfall = float(input("最小雨月の雨量は？(mm)"))
-    if min_rainfall < 0:
-        return f"Err: {err_msg}"
-
+    
     max_rainfall = float(input("最大雨月の雨量は？(mm)")) 
-    if max_rainfall < 0:
-        return f"Err: {err_msg}"
+    
+    if not(min_rainfall < 0 or max_rainfall < 0):
+        f_or_other = min_rainfall*3 < max_rainfall
+    else:
+        return f"Err: {err_msg}" 
+
+    if 8.0 < coldest_temp and warmest_temp < 15.0: # H 高山
+        return "H"
+
+    if coldest_temp < 3.0: # E 寒帯
+        if 0.0 <= warmest_temp < 10.0:
+            return "ET"
+        else:
+            return "EF"
+
+    if coldest_temp < 3.0 and 10.0 < warmest_temp: # D 冷帯
+        if f_or_other:
+            return "Dw"
+        else:
+            return "Df"
 
     if 18.0 <= coldest_temp and 500.0 <= precipitation: # A 熱帯
-        if min_rainfall*3 < max_rainfall:
+        if f_or_other:
             return "Af"
         else:
             return "Aw"
@@ -31,39 +47,24 @@ def devide():
             return "BW"
 
     if -3.0 <= coldest_temp < 18.0 and 500.0 <= precipitation: # C 温帯
-        if min_rainfall*3.0 < max_rainfall and 22.0 < warmest_temp:
+        if f_or_other and 22.0 <= warmest_temp:
             return "Cfa"
-        else:
+        elif f_or_other and warmest_temp < 22:
             return "Cfb"
-
-        def summer_or_winter(answer):
-            if answer == "y":
-                return "Cs"
-            elif answer == "n":
-                return "Cw"
-            else:
-                return "Err"
-
-        month = input("最小雨月の気温は高いですか？[y/n]")
-        result = summer_or_winter(month)
-        while result != "Err":
-            result = summer_or_winter(input("入力を間違えています\n[y/n]？"))
-        return result
-
-    if coldest_temp < 3.0 and 10.0 < warmest_temp: # D 冷帯
-        if min_rainfall*3.0 < max_rainfall:
-            return "Dw"
         else:
-            return "Df"
+            def summer_or_winter(answer):
+                if answer == "y":
+                    return "Cs"
+                elif answer == "n":
+                    return "Cw"
+                else:
+                    return "Err"
 
-    if coldest_temp < 3.0: # E 寒帯
-        if 0.0 <= warmest_temp < 10.0:
-            return "ET"
-        else:
-            return "EF"
-
-    if 8.0 < coldest_temp and warmest_temp < 15.0:
-        return "H"
+            month = input("最小雨月の気温は高いですか？[y/n]")
+            result = summer_or_winter(month)
+            while result != "Err":
+                result = summer_or_winter(input("入力を間違えています\n[y/n]？"))
+            return result
 
     return "Err"
 
